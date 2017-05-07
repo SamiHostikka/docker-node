45dda6fbfd3bb91d7367af18c82c99fb87f4bf29
Sami Hostikka <sami.hostikka@sysart.fi>
2017-05-07 23:35:30 +0300

    Update update script

--- update.sh
+++ update.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 VERSION=$1
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+FILES=Dockerfile*
 
-if [ -z "$VERSION" ]; then
+if [[ -z "$VERSION" ]]; then
   echo "Usage: sh update.sh <Node version>"
   exit 1
 fi
 
-if git show-ref --quiet --verify -- "refs/remotes/origin/$VERSION"; then
-  echo "$VERSION exists already"
-  exit 1
-fi
+for FILE in $FILES; do
+  sed -i "" -e "1 s/[0-9]\{1,\}/$VERSION/g" $FILE
+done
 
-git checkout -b $VERSION
-
-sed -i "s/slim/$VERSION-slim/g" Dockerfile
-sed -i "s/latest/$VERSION/g" onbuild/Dockerfile
-
+git add $FILES
 git commit -am "Update to Node $VERSION"
-git push origin "$VERSION"
-git checkout master
+git push origin $BRANCH
